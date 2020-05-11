cf996cb8d02dcf667556d25b37034c3023946d90
Хусниддинхужа <khusniddinkhuzha@gmail.com>
2020-05-12 01:25:47 +0500

    Update parser_city_russia.py

--- parser_city_russia.py
+++ parser_city_russia.py
@@ -60,7 +60,7 @@ def get_city_russian_wikipedia(all_city_list: dict) -> None:
 
     **all_city_list** — Пустой словарь для сохранения городов.
     """
-    url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8'
+    url = 'https://ru.wikipedia.org/wiki/%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D0%B0_%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD%D0%B0'
     full_html = download_url(url)
     parse_html_city_russian_wikipedia(full_html, all_city_list)
 
